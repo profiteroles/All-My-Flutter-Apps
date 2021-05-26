e62e7dd0c5a78936ede2f992ec26e521e28581f3
profiteroles <erollooper@gmail.com>
2021-05-26 15:28:24 +0800

    BMI

---
+++ Dicee/README.md
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>BMI Calculator App</h2>
+
+<div>
+This app simply calculates your BMI for the given input<br>
+It's got some fancy UI & Calculation function<br>
+<br>
+</div>
+<div class="row">
+<!--   <div class="column"> -->
+    <img src="shot/1.png" alt="Screenshot" width="300" height="600">
+<!--   </div>
+  <div class="column"> -->
+    <img src="shot/2.png" alt="Screenshot" width="300" height="600">
+
+</div>
+</body>
+</html>
---
+++ Dicee/lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDiceNo = 2;
+  int rDiceNo = 1;
+
+  void rollDice() {
+    setState(() {
+      lDiceNo = Random().nextInt(6) + 1;
+      rDiceNo = Random().nextInt(6) + 1;
+    });
+    print('Left Dice is: $lDiceNo');
+    print('Right Dice is : $rDiceNo');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  rollDice();
+                },
+                child: Image.asset('images/dice$lDiceNo.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  rollDice();
+                },
+                child: Image.asset('images/dice$rDiceNo.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
